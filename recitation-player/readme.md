8172cea712dd274d2d22fb59f9ecfa16b6f40c76
fathyElgazar <fathyelgazar348@gmail.com>
2023-08-23 12:04:42 +0300

    Initial commit

---
+++ recitation-player/readme.md
@@ -0,0 +1,12 @@
+## Music Player
+
+Create beautiful UI to play music stored in the "music folder" using the HTML5 audio API
+
+- [👉live demo👈](https://fathyelgazar.github.io/JS-mini-projects/recitation-player)
+
+## Project Specifications
+
+- Create UI for music player including spinning image and song detail popup
+- Add play and pause functionality
+- Switch songs
+- Progress bar
---
+++ recitation-player/script.js
@@ -0,0 +1,106 @@
+const musicContainer = document.getElementById("music-container");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+
+// Song titles
+const songs = ["ibthal", "mad7", "monagah"];
+
+let songIndex = 1;
+
+// Initially load songs to DOM
+loadSong(songs[songIndex]);
+
+// Load song
+function loadSong(song) {
+  title.innerText = `${song}`;
+  audio.src = `music/${song}.m4a`;
+  cover.src = `images/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
+}
+
+// Previous Song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Next Song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Pause Song
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+
+  audio.pause();
+}
+
+// Update Progress Bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+// Event listener
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Change songs
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+// Update progress bar
+audio.addEventListener("timeupdate", updateProgress);
+
+// Set progress bar
+progressContainer.addEventListener("click", setProgress);
+
+// Song end
+audio.addEventListener("ended", nextSong);
